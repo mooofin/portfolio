bded078a95a3b86c31e1265943e35c5bfacfde3c
mooofin <siddharthqln@gmail.com>
2025-08-27 20:08:36 +0530

    load error spotify

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,19 +7,6 @@ function Contact() {
       <p>
         <a href="mailto:siddharthqln@gmail.com" className="back-link">siddharthqln@gmail.com</a>
       </p>
-      <div style={{ marginTop: '1.5rem' }}>
-        <iframe
-          style={{ borderRadius: '12px' }}
-          src="https://open.spotify.com/embed/user/314ikabu7m4lwg6rljojtmhf74uq?utm_source=generator"
-          width="100%"
-          height="352"
-          frameBorder="0"
-          allowFullScreen=""
-          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-          loading="lazy"
-          title="Spotify Profile"
-        ></iframe>
-      </div>
     </section>
   );
 }
