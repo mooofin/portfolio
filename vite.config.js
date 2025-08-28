314c25eeb1df05dd2bfc344657508050dd076f75
mooofin <siddharthqln@gmail.com>
2025-08-28 14:08:30 +0530

    stupid favicon

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  define: {
+    __VITE_DEV_SERVER_URL__: JSON.stringify(''),
+  },
+  clearScreen: false,
 })
